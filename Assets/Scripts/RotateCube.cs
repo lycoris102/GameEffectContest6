424e14b16a0d6018ac6314df2349d2eeaef986f7
Tomoya Aoyagi <lycoris102@gmail.com>
2017-04-05 14:59:49 +0900

    initial commit

---
+++ Assets/Scripts/RotateCube.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class RotateCube : MonoBehaviour
+{
+    public float angleY;
+
+	void Update ()
+	{
+	    this.transform.eulerAngles += new Vector3(0, angleY, 0);
+	}
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# GameEffectContest6
+- https://www.gameeffectcontest.com/
+- マッチロック株式会社主催 第6回ゲームエフェクトコンテスト提出プロジェクト
+  - Unityテクニカル部門
